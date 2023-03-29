fbe567801b7d3ad9c000163fea59615a9aca4fce
王苾留 <2488214334@qq.com>
2023-03-29 17:09:11 +0800

    task 1

--- src/main/java/Track.java
+++ src/main/java/Track.java
@@ -1,19 +1,55 @@
 /**
  * Represents a point in space and time, recorded by a GPS sensor.
  *
- * @author YOUR NAME
+ * @author Wang Biliu
  */
+import java.io.File;
+import java.io.IOException;
+import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 public class Track {
   // TODO: Create a stub for the constructor
+	private List<Point> points;
 
+    public Track(String filename) throws IOException, GPSException {
+        this.points = new ArrayList<>();
+        this.readFile(filename);
+    }
   // TODO: Create a stub for readFile()
-
+	private void readFile(String filename) throws IOException, GPSException {
+        Scanner scanner = new Scanner(new File(filename));
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            String[] parts = line.split(",");
+            if (parts.length != 4) {
+                throw new GPSException("Invalid number of values in line: " + line);
+            }
+            double latitude = Double.parseDouble(parts[0]);
+            double longitude = Double.parseDouble(parts[1]);
+            double elevation = Double.parseDouble(parts[2]);
+            ZonedDateTime timestamp = ZonedDateTime.parse(parts[3]);
+            Point point = new Point(timestamp, longitude, elevation, latitude);
+            this.add(point);
+        }
+        scanner.close();
+    }
   // TODO: Create a stub for add()
-
+    public void add(Point point) {
+        this.points.add(point);
+    }
   // TODO: Create a stub for get()
-
+    public Point get(int index) throws GPSException {
+        if (index < 0 || index >= this.size()) {
+            throw new GPSException("Index out of bounds");
+        }
+        return this.points.get(index);
+    }
   // TODO: Create a stub for size()
-
+    public int size() {
+        return this.points.size();
+    }
   // TODO: Create a stub for lowestPoint()
 
   // TODO: Create a stub for highestPoint()
--- src/main/java/TrackInfo.java
+++ src/main/java/TrackInfo.java
@@ -1,10 +1,30 @@
 /**
  * Program to provide information on a GPS track stored in a file.
  *
- * @author YOUR NAME HERE
+ * @author Wang Biliu
  */
 public class TrackInfo {
   public static void main(String[] args) {
     // TODO: Implementation TrackInfo application here
+	  if (args.length != 1) {}
+	  try {
+		  Track track = new Track(args[0]);
+		  System.out.printf("%d points in track%n", track.size());
+		  
+		  Point lowest = track.lowestPoint();
+		  Point highest = track.highestPoint();
+		  System.out.printf("lowest point is %s%n", lowest);
+		  System.out.printf("Highest point is %s%n", highest);
+		  
+		  double distKm = track.totalDistance() / 1000.0;
+		  System.out.printf("Total distance = %.3f km%n", distKm);
+		  
+		  double avgSpeed = track.averageSpeed();
+		  System.out.printf("Average speed = %3f m/s%n", avgSpeed);
+	  }
+	  catch (Exception error) {
+		  System.err.println("Error: " + error.getMessage());
+		  System.exit(2);
+	  }
   }
 }
